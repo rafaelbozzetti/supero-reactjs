98ab03a0120c0fba01900fe345c8407d5697f9be
Rafael Bozzetti <rafaelbozzetti@gmail.com>
2020-07-08 09:15:00 -0300

    task: initial commit

---
+++ src/components/Detail/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from './style';
+
+
+function Detail() {
+    return (
+      <Container>
+        <h1>Detail</h1>
+      </Container>
+    );
+  }
+  
+  export default Detail;
---
+++ src/components/Search/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from './style';
+
+
+function Search() {
+    return (
+      <Container>
+        <h1>Search</h1>
+      </Container>
+    );
+  }
+  
+  export default Search;
